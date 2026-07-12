255d9501de8e4bd7bcdf6f48459295d16bbf1015
roworu <mamoruessu@gmail.com>
2026-07-13 00:41:59 +0300

    add nvidia script mockup

---
+++ scripts/nvidia.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo subscription-manager repos \
+    --enable codeready-builder-for-rhel-10-$(arch)-rpms
+sudo dnf -y install \
+    https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
+sudo dnf config-manager --add-repo \
+    https://developer.download.nvidia.com/compute/cuda/repos/rhel10/x86_64/cuda-rhel10.repo
+
+sudo dnf install nvidia-open
